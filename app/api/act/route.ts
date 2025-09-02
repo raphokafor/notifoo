6bb9bfa3b9f78aa7e9912d6e06d3720f955482ea
Raphael Okafor Jr <raphael.okafor89@gmail.com>
2025-09-02 14:30:11 -0400

    wip, added some logging

--- app/api/act/route.ts
+++ app/api/act/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: NextRequest) {
   try {
     const { reminderId } = await request.json();
     console.log(
-      "reminderId line 7::::::::::::: reminderId from qstash",
+      "reminderId line 12::::::::::::: reminderId from qstash",
       reminderId
     );
     const reminder = await prisma.reminder.findUnique({
@@ -25,22 +25,31 @@ export async function POST(request: NextRequest) {
       );
     }
 
+    console.log("line 28, reminder", reminder);
+
     // get the user's subscription status
     const subscriptionStatus = reminder?.user?.subscriptionStatus;
 
     // send email
     if (reminder?.emailNotification && subscriptionStatus === "active") {
-      // use the user's email to build the email object
-      const template = NotifyEmailTemplate({
-        reminderName: reminder.name,
-      });
-      await sendEmail({
-        to: reminder?.user?.email,
-        subject: reminder.name,
-        body: template,
-        textBody: template,
-      });
+      try {
+        // use the user's email to build the email object
+        const template = NotifyEmailTemplate({
+          reminderName: reminder.name,
+        });
+        const emailResponse = await sendEmail({
+          to: reminder?.user?.email,
+          subject: reminder.name,
+          body: template,
+          textBody: template,
+        });
+        console.log("line 48, emailResponse", emailResponse);
+      } catch (error) {
+        console.error("Error sending email:::::::::::::::::", error);
+      }
     }
+
+    console.log("line 47, email has been sent");
 
     // send sms
     if (
@@ -48,32 +57,38 @@ export async function POST(request: NextRequest) {
       reminder?.user?.phone &&
       subscriptionStatus === "active"
     ) {
-      // use the user's phone number and convert it to international format
-      const phoneNumber = parsePhoneNumber(
-        reminder?.user?.phone as string,
-        "US"
-      );
-      if (!phoneNumber || !phoneNumber.isValid()) {
-        return NextResponse.json(
-          { message: "Invalid phone number" },
-          { status: 400 }
+      try {
+        // use the user's phone number and convert it to international format
+        const phoneNumber = parsePhoneNumber(
+          reminder?.user?.phone as string,
+          "US"
         );
+        if (!phoneNumber || !phoneNumber.isValid()) {
+          return NextResponse.json(
+            { message: "Invalid phone number" },
+            { status: 400 }
+          );
+        }
+        const smsResponse = await sendTwilioTextMessage({
+          to: phoneNumber.toString(),
+          body: `🔔 Ding ding! Reminder bell says: "${reminder.name}" - Consider this your friendly digital elbow nudge. -Team Notifoo`,
+        });
+        console.log("line 67, smsResponse", smsResponse);
+      } catch (error) {
+        console.error("Error sending sms:::::::::::::::::", error);
       }
-      await sendTwilioTextMessage({
-        to: phoneNumber.toString(),
-        body: `🔔 Ding ding! Reminder bell says: "${reminder.name}" - Consider this your friendly digital elbow nudge. -Team Notifoo`,
-      });
     }
 
     // update the reminder to inactive
-    console.log("line 71, successfully sent email and/or sms ");
-    await prisma.reminder.update({
+    console.log("line 73, successfully sent email and/or sms ");
+    const updatedReminder = await prisma.reminder.update({
       where: { id: reminderId },
       data: { isActive: false },
     });
 
     console.log(
-      "line 79, successfully updated reminder to inactive, reminder job completed"
+      "line 80, successfully updated reminder to inactive, reminder job completed",
+      updatedReminder
     );
 
     // create activity
